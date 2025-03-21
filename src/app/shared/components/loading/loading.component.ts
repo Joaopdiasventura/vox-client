4df648faf39abdb455605d79065e28322462937b
João Paulo Dias <joaopdias.dev@gmail.com>
2025-03-21 08:57:56 -0300

    style: changing the loading format

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'loading',
   template: `
-    <div class="loading-spinner">
-      <div class="spinner"><div class="mid"></div></div>
+    <div class="spinner-container">
+      <span
+        class="spinn-root"
+        role="progressbar"
+        style="transform: none; color: inherit"
+      >
+        <svg class="spinn-svg" viewBox="0 0 50 50">
+          <circle
+            class="spinn-circle circleIndeterminate"
+            style="stroke-dasharray: 80px, 200px; stroke-dashoffset: 0"
+            cx="25"
+            cy="25"
+            r="21.5"
+            fill="none"
+            stroke-width="7.5"
+            stroke="#da2020"
+            stroke-linecap="round"
+          ></circle>
+        </svg>
+      </span>
     </div>
   `,
   styleUrl: './loading.component.scss',
