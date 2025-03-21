fa6779a2fba25009c3371d8aae890753d962b50f
João Paulo Dias <joaopdias.dev@gmail.com>
2025-03-21 09:08:48 -0300

    fix: adjust the render mode form the router /group/:id

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,9 +2,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
-  { path: 'access', renderMode: RenderMode.Server },
+  { path: 'access', renderMode: RenderMode.Prerender },
   { path: 'group/add', renderMode: RenderMode.Prerender },
-  { path: 'group/:id', renderMode: RenderMode.Prerender },
+  { path: 'group/:id', renderMode: RenderMode.Server },
   { path: 'participant/add', renderMode: RenderMode.Prerender },
   { path: 'votes/start', renderMode: RenderMode.Prerender },
   { path: 'votes/follow', renderMode: RenderMode.Prerender },
